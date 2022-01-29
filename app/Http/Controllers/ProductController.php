7203a02bf81e9405febc7d924f80c5e581391fb8
ZyadAshraaf <zyad.ashraaf@gmail.com>
2022-01-29 21:12:03 +0200

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name"=>"required|string",
+            "price"=>"required|numeric"
+        ]);
+        return Product::create($request->all());
+    }
+
+
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            "name"=>"required|string",
+            "price"=>"required|numeric"
+        ]);
+        $product= Product::find($id);
+        $product->update($request->all());
+        return $product;
+    }
+
+
+    public function destroy($id)
+    {
+        return Product::destroy($id);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::post('register',"AuthController@register");
+
+Route::post('login',"AuthController@login");
+
+
+Route::group(['middleware'=>["auth:sanctum"]],function(){
+    Route::resources([
+        "products"=>"ProductController"
+    ]);
+
+    Route::post('logout',"AuthController@logout");
+});
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
